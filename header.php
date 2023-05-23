2da1709db1a2107db13f933022d43004111cd6f5
Sudhanshu Singh <36724360+1SudhanshuSingh@users.noreply.github.com>
2023-05-23 13:00:01 +0530

    Add files via upload
    ADded home apge and bal bala

---
+++ header.php
@@ -0,0 +1,42 @@
+<div class="header sticky-top">
+  <nav class="navbar navbar-expand-lg">
+    <a href="index.php" class="navbar-brand custom-brand-logo">
+      <img src="img/logo.png" alt="" class="logo">
+    </a>
+    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#resp-nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><img src="img/menu.png" alt="" class="nav-img"></span>
+    </button>
+    <div class="collapse text-center mx-auto navbar-collapse" id="resp-nav">
+      <ul class="navbar-nav mx-auto order-0">
+        <li class="nav-item homeBtn">
+          <a class="nav-link white-head" href="index.php">Home</a>
+        </li>
+        <li class="nav-item abtBtn">
+          <a class="nav-link white-head" href="index.php#abt">About Us</a>
+        </li>
+        <li class="nav-item servBtn">
+          <a class="nav-link white-head" href="index.php#serv">Services</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link white-head dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Gallery
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item black-head" href="photography.php">Photography</a>
+            <a class="dropdown-item black-head" href="cinematography.php">Cinematography</a>
+            <a class="dropdown-item black-head" href="graphic.php">Graphic Design</a>
+            <a class="dropdown-item black-head" href="webdev.php">Website Development</a>
+          </div>
+        </li>
+        <li class="nav-item contBtn">
+          <a class="nav-link white-head" href="#cont">Contact Us</a>
+        </li>
+      </ul>
+      <div class="social-icons-nav">
+        <a href="https://www.facebook.com/Amanbhati21/" target="_blank"><img src="img/facebook.png" alt="" class="nav-social-icons"></a>
+        <a href="https://www.instagram.com/amanstudios_/" target="_blank"><img src="img/instagram.png" alt="" class="nav-social-icons"></a>
+        <a href="https://www.youtube.com/channel/UCmBrlS8EqkPGFwrxjgwajCQ" target="_blank"><img src="img/youtube.png" alt="" class="nav-social-icons"></a>
+      </div>
+    </div>
+  </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <meta name="description" content="Aman studios situated at Greater Noida offers services for Photography, Cinematography, Graphic Design and Website Development. Fashion Portfolio, Wedding, Pre Wedding, Product, E Commerce, Baby, Corporate Events, Maternity, Food, Interior, Sports and Architectural. Reach Aman Studios at amanstudios21@gmail.com or +91-9312035007.">
+
+    <meta name="keywords" content="studio in greater noida, photography, fashion, portfolio, wedding, prewedding, product, ecommerce, baby, corporate, events, maternity, food, interior, sports, architectural">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+
+    <!-- OG tags starts-->
+
+    <meta property="og:title" content="Aman Studios | Photography, Cinematography, Graphic Design and Website Development" />
+
+    <meta property="og:type" content="article" />
+
+    <meta property="og:url" content="https://www.amanstudios.in/" />
+
+    <meta property="og:image" content="https://www.amanstudios.in/img/logo.png" />
+
+    <!-- OG tags ends-->
+
+
+
+    <title>Aman Studios Greater Noida | Photography | Cinematography | Graphic Design | Website Development | Reach Aman Studios at amanstudios21@gmail.com or +91-9312035007</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body id="myPage" data-id="1000121545">
+
+    <div class="wrapper">
+
+        <!--header starts-->
+
+        <?php
+
+          include_once 'header.php';
+
+        ?>
+
+        <!--header ends-->
+
+        <!--image banner starts-->
+
+        <div class="img-banner">
+
+          <img src="img/banner1.jpg" alt="">
+
+        </div>
+
+        <!--image banner ends-->
+
+        <!--image carousel starts-->
+
+        <div class="owl-carousel owl-theme">
+
+          
+          <div class="item"><img src="img/slider/11.jpg" alt="prewedding-beach" title="prewedding-shoot1" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/2.jpg" alt="wedding-western" title="prewedding-shoot2" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/3.jpg" alt="reading-girl" title="portfolio-shoot" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/12.jpg" alt="smiling-baby" title="baby-shoot" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/5.jpg" alt="wedding-photoshoot" title="prewedding-shoot3" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/6.jpg" alt="bride-groom-photoshoot" title="bride-groom-photoshoot" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/13.jpg" alt="mom-dad-photoshoot" title="maternity-photoshoot1" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/14.jpg" alt="prewedding-beach" title="maternity-photoshoot2" class="slider-image"></div>
+
+          <div class="item"><img src="img/slider/9.jpg" alt="gym-photoshoot" title="gym-boy-shoot" class="slider-image"></div>
+
+        </div>
+
+        <!--image carousel ends-->
+
+        <!--about us starts-->
+
+        <div id="abt" class="about-us mtop-50px">
+
+          <div class="container-fluid">
+
+            <div class="row">
+
+              <div class="col-lg-8 col-md-6 col-sm-12 col-xs-12">
+
+                <h1 class="text-center custom-heading">Aman Studios</h1>
+
+                <p class="p-10px">Photography is an art of observation. It’s all about finding something interesting in an ordinary place or thing.
+
+
+
+
+
+
+
+To give this art a cutting edge, we at Aman Studios are here to provide you with an everlasting experience of photography. Everyone can take photographs but very few can make it feel like alive. We just do not click photographs or shoot videos, we cast it with life into it. Aman Studios welcomes you to the whole new world of photography and gives you a chance to showcase yourself in front of the camera.</p>
+
+              </div>
+
+              <div class="col-lg-4">
+
+                <img src="img/aman-photo.png" class="aman-img" alt="">
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--about user ends-->
+
+        <!--services starts-->
+
+        <div id="serv" class="services mtop-50px">
+
+          <div class="container-fluid">
+
+            <div class="row">
+
+              <div class="col-lg-12">
+
+                <h1 class="text-center ls-8px">SERVICES</h1>
+
+                <div class="services-cat d-flex flex-wrap justify-content-center">
+
+                  <p class="services-para active">Photography</p>
+
+                  <p class="services-para">Cinematography</p>
+
+                  <p class="services-para">Graphic Design</p>
+
+                  <p class="services-para">Website Development</p>
+
+                </div>
+
+              </div>
+
+              <div class="servicesInner">
+
+                <div class="container-fluid">
+
+                  <div class="seviceInGrd row">
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/maternity/11.jpg" alt="mother-maternity-photoshoot" title="maternity-photoshoot4" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/portfolio/4.jpg" alt="reading-glasses-girl" title="maternity-photoshoot4" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/e-commerce/10.jpg" alt="girl-in-dress" title="ecommerce-photoshoot" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/wedding/5.jpg" alt="smiling-girl" title="prewedding" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/e-commerce/4.jpg" alt="model-girl" title="modelling-photoshoot" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img/photography/wedding/20.jpg" alt="wedding-model" title="wedding-model-photoshoot" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6"><img src="img//photography/wedding/21.jpg" alt="prewedding-model" title="prewedding-model-photoshoot" class="serv-img"></div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-center align-items-center view-more-btn"><a class= "inherit-anc" href="photography.php"><h4 class="view-more">View More</h4></a></div>
+
+                  </div>
+
+                  <div class="seviceInGrd row none">
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <iframe width="100%" height="auto" src="https://www.youtube.com/embed/obNOBWj3iqg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <iframe width="100%" height="auto " src="https://www.youtube.com/embed/ClHw1lWXnkk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <iframe width="100%" height="auto" src="https://www.youtube.com/embed/mMH3qFrT4QI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <iframe width="100%" height="auto" src="https://www.youtube.com/embed/J9p8HYr2sOo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <iframe width="100%" height="auto" src="https://www.youtube.com/embed/EhXNEj-HHGU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-center align-items-center view-more-btn"><a class= "inherit-anc" href="cinematography.php"><h4 class="view-more">View More</h4></a></div>
+
+                  </div>
+
+                  <div class="seviceInGrd row none">
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/logo/melloit.jpg" alt="melloit" title="melloit-logo" class="serv-img logo-img-resp">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/logo/r3_studios.jpg" alt="r3bstudios" title="r3bstudios-logo" class="serv-img logo-img-resp">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/logo/swc_logo.jpg" alt="shantiwellnesscenter" title="shantiwellnesscenter-logo"class="serv-img logo-img-resp">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/logo/textboon.jpg" alt="textboon" title="textboon-logo" class="serv-img logo-img-resp">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-4 offset-lg-4 col-sm-12 d-flex justify-content-center align-items-center mtop-50px">
+
+                      <a class= "inherit-anc" href="graphic.php"><h4 class="view-more">View More</h4></a>
+
+                    </div>
+
+                  </div>
+
+                  <div class="seviceInGrd row none">
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/1.png" alt="banquet-website-template" class="webDesign-img">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/2.png" alt="movie-website-template" class="webDesign-img">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/3.png" alt="jeweller-website-template" class="webDesign-img">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-3 col-md-3 col-sm-6 cake-img">
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/4.jpg" alt="cake-shop-website-landing-page" class="webDesign-img">
+
+                      </div>
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/5.jpg" alt="cake-shop-website-about-us-page" class="webDesign-img">
+
+                      </div>
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/6.png" alt="cake-shop-website-add-cart-page" class="webDesign-img">
+
+                      </div>
+
+                      <div class="col-lg-12">
+
+                        <img src="img/webDesign/7.jpg" alt="cake-shop-website-contact-page" class="webDesign-img">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-lg-4 offset-lg-4 col-sm-12 d-flex justify-content-center align-items-center mtop-50px">
+
+                      <a class= "inherit-anc" href="webdev.php"><h4 class="view-more">View More</h4></a>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--services ends-->
+
+        <!--main Contact starts-->
+
+        <?php
+
+          include_once 'footer.php';
+
+        ?>
+
+        <!--main Contact ends-->
+
+    </div>
+
+
+
+    <script src="js/jquery.min.js" charset="utf-8"></script>
+
+    <script src="js/bootstrap.min.js" charset="utf-8"></script>
+
+    <script src="js/owl.carousel.min.js"></script>
+
+    <script src="js/script.js" charset="utf-8"></script>
+
+    <script type="text/javascript" src="https://archecms.com/dynamic/webData.js"></script>
+
+  </body>
+
+</html>
+<?php 
+	
+?>
+
